ab7f5d4a313f9430ec4512cde150a6f384cd35ec
vcarter <vcarter@udel.edu>
2015-10-10 15:46:15 -0400

    Trying to set up more Wi-Fi P2P code for future

--- app/src/main/java/rediscovery/udel/edu/rediscovery/MainActivity.java
+++ app/src/main/java/rediscovery/udel/edu/rediscovery/MainActivity.java
@@ -1,5 +1,8 @@
 package rediscovery.udel.edu.rediscovery;
 
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,10 +10,29 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final IntentFilter intentFilter = new IntentFilter();
+
     @Override
+    WifiP2pManager.Channel mChannel;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //  Indicates a change in the Wi-Fi P2P status.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+
+        // Indicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+
+        // Indicates the state of Wi-Fi P2P connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+
+        // Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+
     }
 
     @Override
@@ -33,5 +55,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        receiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
+        registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
     }
 }
